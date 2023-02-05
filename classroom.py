28371c0bfa787ec22246e52e9fc6721207d9ea29
dev_winston <juliuswinston7@gmail.com>
2023-02-05 20:06:03 +0000

    added to list

--- classroom.py
+++ classroom.py
@@ -1,24 +1,3 @@
-# LIST AND TUPLES
-
-# List can hold multiple values of the same data type of different data types
-# Values in a list a called items
-# The items are separated by comas (coma-delimited)
-# Items in a list are ordered
-
-# names_of_gf1 = "Adwoa"
-# names_of_gf2 = "Ama"
-# names_of_gf3 = "Akos"
-# names_of_gf4 = "Yaayaa"
-# names_of_gf5 = "Abena"
-
-    # Item       1       2       3        4        5
-tsatsu_gfs = ["Adwoa", "Ama", "Akos", "Yaayaa", "Abena"]
-    # Indexes    0       1       2        3        4
-    # Indexes   -5      -4      -3       -2       -1
- 
-# NEGATIVE
-print(tsatsu_gfs[-4])
-
 # Getting a list from another list with slices
 
 # pets = ["bat", "cat", "elephant", "rat"]
